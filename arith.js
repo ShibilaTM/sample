1f8d9590ad7fb3de93d27c49bebe4ad4e964a4d6
ShibilaTM <shibilaang@gmail.com>
2023-08-03 23:32:34 +0530

    project added

---
+++ arith.js
@@ -0,0 +1,12 @@
+let x=30;
+let y=10;
+let res=x+y;
+console.log(res);
+let res1=x-y;
+console.log(res1);
+let res2=x*y;
+console.log(res2);
+let res3=x/y;
+console.log(res3);
+let res4=x%y;
+console.log(res4);
